e8404e60971f0944076e4b5aed38f885823f2e30
Amin Kodaganur <aminkodaganur@gmail.com>
2016-06-05 13:23:11 +0530

    new changes added

--- dist/assets/js/custom.js
+++ dist/assets/js/custom.js
@@ -1,5 +1,56 @@
 $(document).ready(function() {
 	
+	$('.modal-trigger').on('click', function(event) {
 
-	alert('Jquery is ready');
+		var targetModal = $(this).data('target');
+		var a = $(targetModal).hasClass('active');
+		if(a){			
+			$(targetModal).addClass('animated fadeOutDown')
+						  .one(
+							'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+						   	function(){
+						   		$(targetModal).removeClass('animated fadeOutDown');
+						   		$(targetModal).removeClass('active');
+						   	});
+			
+		}else{
+			$(targetModal).addClass('active');
+			$(targetModal).addClass('animated fadeInUp')
+						  .one(
+							'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+						   	function(){
+						   		$(targetModal).removeClass('animated fadeInUp');
+						   	});
+			var active_tab = $(this).data('tab-target');
+			$('.am-tab.am-tab-containt').removeClass('active');
+			$('.am-tab.am-tab-containt[data-tab='+active_tab+']').addClass('active');
+
+			$('.am-tab .item').removeClass('active');
+			$('.am-tab .item[data-tab-target='+active_tab+']').addClass('active'); 
+		}
+	});
+
+	$('.close-tab').on('click', function(event) {
+		var targetModal = $(this).data('target');		
+		$(targetModal).addClass('animated fadeOutDown')
+					  .one(
+						'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+					   	function(){
+					   		$(targetModal).removeClass('animated fadeOutDown');
+					   		$(targetModal).removeClass('active');
+					   	});
+	});
+
+	$('.am-tab .item').on('click', function(event) {
+		$('.am-tab .item').removeClass('active');
+		$(this).addClass('active');
+		var active_tab = $(this).data('tab-target');
+		$('.am-tab.am-tab-containt').removeClass('active');
+		$('.am-tab.am-tab-containt[data-tab='+active_tab+']').addClass('active');
+	});
+
+	$('#trigger-att').on('click', function(event) {
+		$('#attachment').trigger('click')
+	});
+
 });
